2b5c9906ba429f410963f5ed69b28a71796072b1
Vishesh Jyani <vishesh.jyani@sprinklr.com>
2022-08-21 01:36:44 +0530

    Add item form - non functional

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import groceryItems, { GroceryItem } from "./data.js";
 
 const groceryListItems = document.querySelector('.grocery-list-items')
-
+// Display static grocery items from dataset
 groceryItems.forEach(item => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `
@@ -19,8 +19,25 @@ groceryItems.forEach(item => {
 
 const removeButtons = document.querySelectorAll(".remove-button")
 
+// Remove logic
 function removeItem(e) {
     const listItem = e.target.parentNode;
     listItem.remove();
 }
+
 removeButtons.forEach(button => button.addEventListener("click", removeItem))
+
+// Add logic
+const addItemButton = document.querySelector(".add-item")
+
+const addItemForm = document.querySelector('.add-item-form');
+
+addItemButton.addEventListener('click', (e)=>{
+    e.stopPropagation()
+    addItemForm.style.display = 'block'
+})
+
+document.addEventListener('click', (e)=>{
+    if(e.target.closest('form') !== addItemForm)
+        addItemForm.style.display = 'none'
+})
